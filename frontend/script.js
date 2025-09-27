5411f9f911357e0c53af8ad11509d7f90d6a54e6
Sumit <sumitnautiyal681@gmail.com>
2025-09-27 20:40:34 +0530

    Fix Contact Form fetch URL for live Deployment

--- frontend/script.js
+++ frontend/script.js
@@ -78,7 +78,7 @@ form.addEventListener("submit", async (e) => {
     };
 
     try {
-        const res = await fetch("http://localhost:5000/api/contact", {
+        const res = await fetch("/api/contact", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData)
